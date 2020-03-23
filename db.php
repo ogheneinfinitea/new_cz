438a6660396f9f9aa29a8cb1b69b37d747c16288
Samson Ogheneriye Eduotu <ogheneinfinitea@gmail.com>
2020-03-23 21:10:26 +0000

    created new database configuration

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 <?php
 $hostname = "localhost";
 $user ="root";
-$db_name ="bitblarg";
+$db_name ="cz";
 $password ="";
 $mysqli = mysqli_connect($hostname,$user,$password,$db_name);
 if(!$mysqli) {
