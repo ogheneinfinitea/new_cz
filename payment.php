e71a0e0f70b7826cfd640022dfa0bfef0ef18af7
Samson Ogheneriye Eduotu <ogheneinfinitea@gmail.com>
2020-03-25 17:59:14 +0000

    added payment.php

---
+++ payment.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="CryptoCoin is a powerful Bitcoin Crypto Currency Wallet and Mining Template with full of customization options and features">
+		<!-- Fav Icon  -->
+		<link rel="shortcut icon" href="pix/favicon1.png">
+		<!-- Site Title  -->
+		<title>Cztradingoptions |  PAYMENT </title>
+		<!-- Vendor Bundle CSS -->
+		<link rel="stylesheet" href="assets/css/vendor.bundle.css" >
+		<!-- Custom styles for this template -->
+		<link href="assets/css/style.css" rel="stylesheet">
+		<link href="assets/css/theme.css" rel="stylesheet">
+		
+	</head>
+	<body>
+	
+		<!-- Header --> 
+		<?php include("includes/header.php")?>
+			<!-- #end Navbar -->
+			<!-- Banner/Slider -->
+			<div class="page-head section row-vm light">
+				<div class="imagebg">
+					<img src="pix/about.jpg" alt="page-head">
+				</div>
+				<div class="container">
+					<div class="row text-center">
+						<div class="col-md-12">
+							<h2>About Us</h2>
+							<div class="page-breadcrumb">
+								<ul class="breadcrumb">
+									<li><a href="index.php">Home</a></li>
+									<li class="active"><span>About Us</span></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- #end Banner/Slider -->
+		</header>
+    		
+     	<!--Section -->
+       	<div class="section section-pad">
+       		<div class="container">
+       			<div class="row row-vm">
+       				<div class="col-md-5 res-m-bttm">
+						<div class="text-block">
+							<h2>MAKE <span>PAYMENT</span></h2>
+							<p align="justify">Cztradingoptions is a true opportunity to earn on cryptocurrency.
+								Nowadays cryptocurrency is one of the main payment instruments, which can be used online. Cztradingoptions has been using this kind of digital payment for quite a while to gain and raise profit.</p>
+
+							
+							<ul>
+								<li>Login to your acccount  <a href="http://localhost/new_cz/login.php">Account Login</a>.</li>
+								<li>Click on Buy and Sell Button</li>
+								<li> Click on Deposit  </li>
+								<li>Enter Preferred Amount in Pounds (Bitcoin Equivalence)</li>
+                                <li>Click on Deposit</li>
+                                <li>Make Payment into the Displayed Bitcoin Address or Scan the QR code</li>
+							</ul>
+							
+						</div>
+       				</div>
+       				<div class="col-md-6 col-md-offset-1">
+       					<div class="video-block round">
+       						<img src="pix/bitrediff_two.jpg" alt="Bitrediff" class="">
+       						
+       					</div>
+       				</div>
+       			</div>
+       		</div>
+       	</div>
+       	<!--End Section -->
+       	
+       	<!--Section -->
+       	<div class="section section-pad bg-grey">
+       		<div class="container">
+       			<div class="section-head">
+					<div class="row text-center">
+						<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+							<h2 class="heading-section">Why choose Us</h2>
+							<p>Our mining pool has been in operation since 2014. Since then we’ve deployed 500 petahash (514.84 PH/s) and we’ve mined more than
+								 70,000 coins—we invest in even more resources, servers, and high-end mining software to scale up operations to continue to deliver results.</p>
+						</div>
+					</div>
+       			</div>
+       			<div class="gaps size-3x"></div>
+				<div class="row text-center">
+					<div class="col-md-4 res-m-bttm-lg">
+						<div class="box-alt">
+							<div class="image-icon">
+								<img src="images/box-icon-f.png" alt="box-icon">
+							</div>
+							<h4>Payment Options</h4>
+							<!-- <p>Morbi eget varius risus, ut venenatis libero Pellentesque in porta dui.</p> -->
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="box-alt">
+							<div class="image-icon">
+								<img src="images/box-icon-g.png" alt="box-icon">
+							</div>
+							<h4>Strong Security</h4>
+							<!-- <p>Morbi eget varius risus, ut venenatis libero Pellentesque in porta dui.</p> -->
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="box-alt">
+							<div class="image-icon">
+								<img src="images/box-icon-h.png" alt="box-icon">
+							</div>
+							<h4>World Coverage</h4>
+							<!-- <p>Morbi eget varius risus, ut venenatis libero Pellentesque in porta dui.</p> -->
+						</div>
+					</div>
+				</div>
+       		</div>
+       	</div>
+       	<!--End Section -->
+       	
+       	<!--Section -->
+       <!-- 	<div class="section section-pad">
+       		<div class="container">
+       			<div class="section-head">
+					<div class="row text-center">
+						<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+							<h2 class="heading-section">what you can do ?</h2>
+							<p>Sed ut perspi ciatis unde omnis iste natus error sit volup tatem accusa ntium dolor emque lauda ntium, totam rem aperiam</p>
+						</div>
+					</div>
+       			</div>
+       			<div class="gaps size-3x"></div>
+       			<div class="row text-center">
+       				<div class="col-md-4 col-sm-6">
+       					<div class="service-box shadow round">
+       						<div class="service-thumb">
+       							<a href="service-single.html"><img src="images/service-thumb-a.jpg" alt="service"></a>
+       						</div>
+       						<div class="service-entry">
+       							<h5>Buy Bitcoin Online</h5>
+       							<p>Justo dolor pede pede sit. Eu amet eos mauris, iaculis in, fringilla diam eros erat, fermentum etiam parturient est adipiscing</p>
+       							<a href="service-single.html" class="btn-icon"><span class="pe pe-7s-angle-right"></span></a>
+       						</div>
+       					</div>
+						<div class="gaps size-3x"></div>
+       				</div>
+       				<div class="col-md-4 col-sm-6">
+       					<div class="service-box shadow round">
+       						<div class="service-thumb">
+       							<a href="service-single.html"><img src="images/service-thumb-b.jpg" alt="service"></a>
+       						</div>
+       						<div class="service-entry">
+       							<h5>sell bitcoin</h5>
+       							<p>Justo dolor pede pede sit. Eu amet eos mauris, iaculis in, fringilla diam eros erat, fermentum etiam parturient est adipiscing</p>
+       							<a href="service-single.html" class="btn-icon"><span class="pe pe-7s-angle-right"></span></a>
+       						</div>
+       					</div>
+						<div class="gaps size-3x"></div>
+       				</div>
+       				<div class="col-md-4 col-sm-6">
+       					<div class="service-box shadow round">
+       						<div class="service-thumb">
+       							<a href="service-single.html"><img src="images/service-thumb-c.jpg" alt="service"></a>
+       						</div>
+       						<div class="service-entry">
+       							<h5>earn passive income</h5>
+       							<p>Justo dolor pede pede sit. Eu amet eos mauris, iaculis in, fringilla diam eros erat, fermentum etiam parturient est adipiscing</p>
+       							<a href="service-single.html" class="btn-icon"><span class="pe pe-7s-angle-right"></span></a>
+       						</div>
+       					</div>
+						<div class="gaps size-3x"></div>
+       				</div>
+       				<div class="col-md-4 col-sm-6 res-m-bttm-3x">
+       					<div class="service-box shadow round">
+       						<div class="service-thumb">
+       							<a href="service-single.html"><img src="images/service-thumb-d.jpg" alt="service"></a>
+       						</div>
+       						<div class="service-entry">
+       							<h5>trade with sceure escrow</h5>
+       							<p>Justo dolor pede pede sit. Eu amet eos mauris, iaculis in, fringilla diam eros erat, fermentum etiam parturient est adipiscing</p>
+       							<a href="service-single.html" class="btn-icon"><span class="pe pe-7s-angle-right"></span></a>
+       						</div>
+       					</div>
+       				</div>
+       				<div class="col-md-4 col-sm-6 res-m-bttm-3x">
+       					<div class="service-box shadow round">
+       						<div class="service-thumb">
+       							<a href="service-single.html"><img src="images/service-thumb-e.jpg" alt="service"></a>
+       						</div>
+       						<div class="service-entry">
+       							<h5>build your reputation</h5>
+       							<p>Justo dolor pede pede sit. Eu amet eos mauris, iaculis in, fringilla diam eros erat, fermentum etiam parturient est adipiscing</p>
+       							<a href="service-single.html" class="btn-icon"><span class="pe pe-7s-angle-right"></span></a>
+       						</div>
+       					</div>
+       				</div>
+       				<div class="col-md-4 col-sm-6 res-m-bttm-3x">
+       					<div class="service-box shadow round">
+       						<div class="service-thumb">
+       							<a href="service-single.html"><img src="images/service-thumb-f.jpg" alt="service"></a>
+       						</div>
+       						<div class="service-entry">
+       							<h5>get a free wallet</h5>
+       							<p>Justo dolor pede pede sit. Eu amet eos mauris, iaculis in, fringilla diam eros erat, fermentum etiam parturient est adipiscing</p>
+       							<a href="service-single.html" class="btn-icon"><span class="pe pe-7s-angle-right"></span></a>
+       						</div>
+       					</div>
+       				</div>
+       			</div>
+       		</div>
+       	</div> -->
+       	<!--End Section -->
+       	
+       	
+    	<!-- Section -->
+    	<div class="section section-pad cta-section light has-bg dark-filter">
+    		<div class="imagebg has-parallax">
+				<img src="pix/bitrediff_buy.jpg" alt="Bitrediff">
+			</div>
+    		<div class="container">
+    			<div class="row text-center">
+    				<div class="col-md-8 col-md-offset-2">
+    					<h3>Are you searching for a quick, cheap, and safe way to buy Bitcoins? </h3>
+    					<a href="buy-sell.php" class="btn btn-md">buy bitcoin</a>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+     	<!-- End Section -->
+      	
+       	<!--Section -->
+       	<div class="section section-pad">
+       		<div class="container">
+       			<div class="section-head">
+					<div class="row text-center">
+						<div class="col-md-6 col-md-offset-3 col-md-offset-3 col-sm-8 col-sm-offset-2">
+							<h2 class="heading-section">our expert team</h2>
+							<p></p>
+						</div>
+					</div>
+       			</div>
+       			<div class="gaps size-3x"></div>
+       			<div class="row text-center">
+       				<div class="col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2 res-m-bttm">
+       					<div class="team-member">
+       						<div class="team-thumb">
+       							<img src="pix/bitrediff_team1.jpg" alt="team-thumb">
+       							<div class="team-thumb-overlay">
+       								<!-- <ul class="team-social">
+       									<li><a href="#"><span class="fa fa-facebook"></span></a></li>
+       									<li><a href="#"><span class="fa fa-twitter"></span></a></li>
+       									<li><a href="#"><span class="fa fa-instagram"></span></a></li>
+       								</ul> -->
+       							</div>
+       						</div>
+       						<div class="team-info">
+       							<h5 class="team-name">jonson makerson</h5>
+       							<span class="team-title">consultant</span>
+       						</div>
+       					</div>
+       				</div>
+       				<div class="col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2 res-m-bttm">
+       					<div class="team-member">
+       						<div class="team-thumb">
+       							<img src="pix/bitrediff_team2.png" alt="team-thumb">
+       							<div class="team-thumb-overlay">
+       								<!-- <ul class="team-social">
+       									<li><a href="#"><span class="fa fa-facebook"></span></a></li>
+       									<li><a href="#"><span class="fa fa-twitter"></span></a></li>
+       									<li><a href="#"><span class="fa fa-instagram"></span></a></li>
+       								</ul> -->
+       							</div>
+       						</div>
+       						<div class="team-info">
+       							<h5 class="team-name">nikon tupora</h5>
+       							<span class="team-title">team leader</span>
+       						</div>
+       					</div>
+       				</div>
+       				<div class="col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2 res-m-bttm">
+       					<div class="team-member">
+       						<div class="team-thumb">
+       							<img src="pix/bitrediff_team3.jpg" alt="team-thumb">
+       							<div class="team-thumb-overlay">
+       								<!-- <ul class="team-social">
+       									<li><a href="#"><span class="fa fa-facebook"></span></a></li>
+       									<li><a href="#"><span class="fa fa-twitter"></span></a></li>
+       									<li><a href="#"><span class="fa fa-instagram"></span></a></li>
+       								</ul> -->
+       							</div>
+       						</div>
+       						<div class="team-info">
+       							<h5 class="team-name">jahidul hossen</h5>
+       							<span class="team-title">Broker</span>
+       						</div>
+       					</div>
+       				</div>
+       			</div>
+       		</div>
+       	</div>
+       	<!--End Section -->
+       	
+       	<!--Section -->
+		   <div class="section section-pad bg-grey">
+       		<div class="container">
+       			<div class="section-head">
+					<div class="row text-center">
+						<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+							<h2 class="heading-section">What investors say</h2>
+							<p>“The Bitcoin protocol has immense intrinsic value as a self-regulating frictionless payment network affordable to almost anyone. Here is a technology that allows anyone to send any amount of money to anyone else in the world at virtually no cost with nothing more than an Internet connection or smart phone. </p>
+						</div>
+					</div>
+       			</div>
+       			<div class="gaps size-3x"></div>
+       			<div class="row text-center">
+       				<div class="col-md-6 col-md-offset-3">
+       					<div class="testimonial-carousel has-carousel" data-items="1" data-loop="true" data-dots="true" data-auto="true">
+       						<div class="testimonial-item">
+       							<div class="client-photo circle">
+       								<img src="pix/investor1.jpg" alt="client">
+       								<em class="fa fa-quote-right"></em>
+       							</div>
+       							<blockquote>Bitcoin, like the Internet, is one of those innovations that can break down barriers; information barriers in the case of the Internet, and financial barriers with Bitcoin.</blockquote>
+       							<div class="client-info">
+       								<h6>Armaan Mitchell</h6>
+       								<span>CEO, Company Name</span>
+       							</div>
+       						</div>
+       						<div class="testimonial-item">
+       							<div class="client-photo circle">
+       								<img src="pix/investor2.jpg" alt="client">
+       								<em class="fa fa-quote-right"></em>
+       							</div>
+       							<blockquote>Bitcoin mining is the only way to introduce new currency to the marketplace, and it is performed by "miners" who use expensive software to solve math problems in exchange for the currency.</blockquote>
+       							<div class="client-info">
+       								<h6>Cheryl J. Sanders</h6>
+       								<span></span>
+       							</div>
+       						</div>
+       					</div>
+       				</div>
+       			</div>
+       		</div>
+       	</div>
+       	<hr>
+       	<!--End Section -->
+       	
+       	<!-- Section -->
+		<!-- <div class="section section-pad-md">
+			<div class="container">
+				<div class="content row">
+					<div class="owl-carousel has-carousel no-dots"  data-items="6" data-loop="true" data-dots="false" data-auto="false">
+						<div class="logo-item"><img alt="" width="190" height="82" src="images/cl-logo1.png"></div>
+						<div class="logo-item"><img alt="" width="190" height="82" src="images/cl-logo2.png"></div>
+						<div class="logo-item"><img alt="" width="190" height="82" src="images/cl-logo3.png"></div>
+						<div class="logo-item"><img alt="" width="190" height="82" src="images/cl-logo4.png"></div>
+						<div class="logo-item"><img alt="" width="190" height="82" src="images/cl-logo5.png"></div>
+						<div class="logo-item"><img alt="" width="190" height="82" src="images/cl-logo6.png"></div>
+					</div>
+
+				</div>
+			</div>	
+		</div> -->
+		<!-- End Section -->
+		
+       	<!-- Section Footer -->
+		<?php include("includes/footer.php")?>
+		<!-- Preloader End -->
+		
+		<!-- JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		
+		<script src="assets/js/jquery.bundle.js"></script>
+		<script src="assets/js/script.js"></script>
+		
+	</body>
+</html>
